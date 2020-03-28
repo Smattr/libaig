018b969e34d175dd223d579ae16f86f3b5b478c9
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-28 10:22:34 -0700

    implement a simple application for testing libaig

---
+++ aig-ls/main.c
@@ -0,0 +1,31 @@
+// simple application for reading an AIG header, to test libaig
+
+#include <aig/aig.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s filename\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  aig_t *aig = NULL;
+  int rc = aig_load(&aig, argv[1]);
+  if (rc != 0) {
+    perror("aig_load");
+    return EXIT_FAILURE;
+  }
+
+  printf("M = %" PRIu64 "\n", aig_max_index(aig));
+  printf("I = %" PRIu64 "\n", aig_input_count(aig));
+  printf("L = %" PRIu64 "\n", aig_latch_count(aig));
+  printf("O = %" PRIu64 "\n", aig_output_count(aig));
+  printf("A = %" PRIu64 "\n", aig_and_count(aig));
+
+  aig_free(&aig);
+
+  return EXIT_SUCCESS;
+}
